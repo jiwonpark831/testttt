7988a285e1bad56c8bbd8863ebd8271fd5334389
jiwonpark831 <jiwonpark831@gmail.com>
2025-05-25 21:17:20 +0900

    aasasas

--- testttt/ContentView.swift
+++ testttt/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                        Text("Item atsssss \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                        Text("Item atssssssssss \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
                     } label: {
                         Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
                     }
